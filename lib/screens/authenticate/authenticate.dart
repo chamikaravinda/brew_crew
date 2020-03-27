99246f21e97bdfa36b7cd79ced3af493eef0217a
Chamika Ravinda <chamikaravinda@gmail.com>
2020-03-27 19:32:45 +0530

    register and sign up

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -1,3 +1,4 @@
+import 'package:brewcrew/screens/authenticate/register.dart';
 import 'package:brewcrew/screens/authenticate/sign_in.dart';
 import 'package:flutter/material.dart';
 
@@ -10,7 +11,7 @@ class _AuthernticateState extends State<Authernticate> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: SignIn(),
+      child: Register(),
     );
   }
 }

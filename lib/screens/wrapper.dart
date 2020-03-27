3379b26ecaa48d834b4b202878fe493ba0b75260
Chamika Ravinda <chamikaravinda@gmail.com>
2020-03-27 15:25:49 +0530

    sign out

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,13 +1,18 @@
 import 'package:brewcrew/screens/authenticate/authenticate.dart';
 import 'package:brewcrew/screens/home/home.dart';
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
+import 'package:brewcrew/models/user.dart';
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //TODO : return either home of authenticate widget
-    return Authernticate(
 
-    );
+    final user = Provider.of<User>(context);
+    //return either home of authenticate widget
+    if(user == null){
+      return Authernticate();
+    }else{
+      return Home();
+    }
   }
 }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,15 +1,28 @@
+import 'package:brewcrew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService{
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  //TODO:sign in anon
+  //create user obj sed on FirebaseUser
+  User _userFromFirebaseUser(FirebaseUser user){
+    return user != null ? User(uid:user.uid):null;
+  }
+
+  //auth change user stream
+  Stream<User> get user{
+    return _auth.onAuthStateChanged
+        //.map((FirebaseUser user) => _userFromFirebaseUser(user));
+        .map(_userFromFirebaseUser);
+  }
+
+  //sign in anon
   Future signInAnon() async{
     try{
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     }catch(e){
       print(e.toString());
       return null;
@@ -20,6 +33,14 @@ class AuthService{
 
   //TODO:register with email & password
 
-  //TODO:sign out
+  //sign out
+  Future signOut() async{
+    try{
+      return await _auth.signOut();
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
 
 }

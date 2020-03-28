77c5438772d122c7dd8284278607fdc49c8a95f1
Chamika Ravinda <chamikaravinda@gmail.com>
2020-03-28 17:48:44 +0530

    create stream to see changing data

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,26 +1,34 @@
 import 'package:brewcrew/services/auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
+import 'package:brewcrew/services/database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:brewcrew/screens/home/brew_list.dart';
 class Home extends StatelessWidget {
 
   final AuthService _authService = AuthService();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: Text('Brew Crew'),
-        backgroundColor:  Colors.brown[400],
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
-              onPressed: () async{
-                await _authService.signOut();
-              },
-              icon: Icon(Icons.person,color: Colors.white,),
-              label: Text('Log out',style: TextStyle(color:Colors.white)),
-          )
-        ],
+    return StreamProvider<QuerySnapshot>.value(
+      value: DatabaseService().brews,
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text('Brew Crew'),
+          backgroundColor:  Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+                onPressed: () async{
+                  await _authService.signOut();
+                },
+                icon: Icon(Icons.person,color: Colors.white,),
+                label: Text('Log out',style: TextStyle(color:Colors.white)),
+            )
+          ],
+        ),
+        body: BrewList(),
       ),
     );
   }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,4 +1,5 @@
 import 'package:brewcrew/models/user.dart';
+import 'package:brewcrew/services/database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService{
@@ -45,6 +46,8 @@ class AuthService{
     try{
       AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
       FirebaseUser user =  result.user;
+      //create a new document for the user with the uid
+      await DatabaseService(uid: user.uid).updateUserData('0', 'new crew member', 100);
       return _userFromFirebaseUser(user);
     }catch(e){
       print(e.toString());

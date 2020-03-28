a39ee9ebb503ddebd59490d81fefe673df9e4558
Chamika Ravinda <chamikaravinda@gmail.com>
2020-03-28 11:37:29 +0530

    sign in done

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -14,10 +14,13 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
 
   final AuthService _authService = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   //text filed state
   String email = '';
   String password = '';
+  String error ='';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,16 +47,19 @@ class _SignInState extends State<SignIn> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               SizedBox(height: 20.0,),
               TextFormField(
+                validator: (val)=>val.isEmpty ? 'Enter an email' :null,
                 onChanged: (val){
                   setState(() => email=val);
                 },
               ),
               SizedBox(height: 20.0,),
               TextFormField(
+                validator: (val)=>val.length<6 ? 'Enter a password longer than 6 characters' :null,
                 obscureText: true,
                 onChanged: (val){
                   setState(() => password=val);
@@ -67,9 +73,21 @@ class _SignInState extends State<SignIn> {
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
-
+                  if(_formKey.currentState.validate()){
+                    dynamic result = await _authService.signInWithEmailAndPassword(email, password);
+                    if(result == null){
+                      setState(() {
+                        error ='Wrong email or password';
+                      });
+                    }
+                  }
                 },
               ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red,fontSize: 14.0),
+              )
             ],
           ),
         ),
